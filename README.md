61618dd9701b21b626e9cef9b7cff25059adafcc
Porto-Yospunya <yospunyaporto@gmail.com>
2024-09-13 13:00:57 +0700

    update folder and file
    - init node js
    - add file server
    - add folder
    - add navbar.ejs and navbar.css

---
+++ README.md
@@ -0,0 +1,6 @@
+# Install node_modules
+npm i
+
+# Run website
+
+npm run dev
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const server = express();
+
+server.set("view engine", "ejs");
+
+server.use(express.json());
+server.use(express.static(path.join(__dirname, "public")));
+server.use(express.urlencoded({ extended: true }))
+
+server.get('/', (req, res) => {
+    res.render('pages/home');
+});
+
+server.get('/gallery', (req, res) => {
+    res.render('pages/gallery');
+});
+
+server.get('/menu', (req, res) => {
+    res.render('pages/menu');
+});
+
+const PORT = 5050 || 5051;
+
+server.listen(PORT, () => {
+    console.log(`Server running successfully on port: ${PORT}`);
+});
