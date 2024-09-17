46e0fc652e444835d6cf821b99e7081997b4af45
Porto-Yospunya <yospunyaporto@gmail.com>
2024-09-17 12:48:48 +0700

    update menu
    - add hotdeal in menu page (not responsive)

---
+++ public/js/menu-script.js
@@ -0,0 +1,12 @@
+let hnext = document.querySelector('.hotdeal-next')
+let hprev = document.querySelector('.hotdeal-prev')
+
+hnext.addEventListener('click', function(){
+    let hitems = document.querySelectorAll('.hotdeal-item')
+    document.querySelector('.hotdeal-slide').appendChild(hitems[0])
+})
+
+hprev.addEventListener('click', function(){
+    let hitems = document.querySelectorAll('.hotdeal-item')
+    document.querySelector('.slide').prepend(hitems[hitems.length - 1]) // here the length of items = 6
+})
